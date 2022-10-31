d59ff370f030ca7b0595c1a619b3fca10226520e
Viviana Arango <vivianaarango@Vivianas-MacBook-Pro.local>
2022-10-31 11:41:37 -0600

    feat: create vertical and horizontal validation

--- main.go
+++ main.go
@@ -20,40 +20,75 @@ func isMutant(adn []string) bool {
 			mutantADN[i][j] = separatedADN[j]
 		}
 
-		//fmt.Println(mutantADN[i])
+		fmt.Println(mutantADN[i])
 	}
 
-	fmt.Println("aqui", validateHorizontal(mutantADN))
+	fmt.Println("Vertical:", vertical(mutantADN))
+	fmt.Println("Horizontal:", horizontal(mutantADN))
 
-	return true
+	if horizontal(mutantADN) > 1 || vertical(mutantADN) > 1 {
+		return false
+	}
+
+	if horizontal(mutantADN) == 1 || vertical(mutantADN) == 1 {
+		return true
+	}
+
+	return false
 }
 
-func validateHorizontal(mutantADN [][]string) bool {
+func horizontal(mutantADN [][]string) int {
 	countValid := 0
-	for i := 0; i < len(mutantADN); i++ {
-		coincidences := 0
+	for r := 0; r < len(mutantADN); r++ {
+		coincidences := 1
 		currentValue := ""
-		for j := 0; j < len(mutantADN[i])-1; j++ {
+
+		for c := 0; c < len(mutantADN[r])-1; c++ {
 			if currentValue == "" {
-				currentValue = mutantADN[i][j]
+				currentValue = mutantADN[r][c]
 			}
 
-			if currentValue == mutantADN[i][j] {
+			if currentValue == mutantADN[r][c+1] {
 				coincidences++
+			} else {
+				currentValue = ""
 			}
 
 			if coincidences == 4 {
 				countValid++
 				coincidences = 0
 			}
+		}
+	}
 
-			if countValid > 1 {
-				return false
+	return countValid
+}
+
+func vertical(mutantADN [][]string) int {
+	countValid := 0
+	for i := 0; i < len(mutantADN); i++ {
+		coincidences := 1
+		currentValue := ""
+
+		for j := 0; j < len(mutantADN)-1; j++ {
+			if currentValue == "" {
+				currentValue = mutantADN[j][i]
+			}
+
+			if currentValue == mutantADN[j+1][i] {
+				coincidences++
+			} else {
+				currentValue = ""
+			}
+
+			if coincidences == 4 {
+				countValid++
+				coincidences = 0
 			}
 		}
 	}
 
-	return true
+	return countValid
 }
 
 func isValidADN(rowADN string) bool {
@@ -70,7 +105,7 @@ func isValidADN(rowADN string) bool {
 
 func main() {
 	data := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
-	//data := []string{"AAAAAE"}
+
 	result := isMutant(data)
 
 	if result {
